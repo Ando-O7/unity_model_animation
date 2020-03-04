87e71cba3e13d1281ae4a1e520e2d26d8094a6cb
ando07 <ando.masahito@gmail.com>
2020-03-04 22:24:16 +0900

    [add]wolf animation script

---
+++ Assets/WolfScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfScript : MonoBehaviour {
+
+	private Animation anim;
+	private bool walkMode;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (walkMode)
+		{
+			transform.Translate(Vector3.forward * Time.deltaTime * (transform.localScale.x * .1f));
+		}
+	}
+
+	public void RunAction()
+	{
+		anim.Play ("Wolf_Skeleton|Wolf_Run_Cycle_");
+		walkMode = false;
+	}
+
+	public void SeatAction()
+	{
+		anim.Play("Wolf_Skeleton|Wolf_seat_");
+		walkMode = false;
+	}
+
+	public void WalkAction()
+	{
+		anim.Play ("Wolf_Skeleton|Wolf_Walk_cycle_");
+		walkMode = true;
+	}
+}
